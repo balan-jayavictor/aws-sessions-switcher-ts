7d94c755c7d704b803c5262cbddcdb6b92eba7bf
Balan Jayavictor <balan.jayavictor@dearhealth.com>
2025-04-10 12:59:43 +0200

    chore: updated eslint config

---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+// @ts-check
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+module.exports = [
+  {
+    ignores: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/*.config.js',
+      '.eslintrc.js', 
+      'jest.config.js'
+    ],
+  },
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: require('@typescript-eslint/parser'),
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': require('@typescript-eslint/eslint-plugin'),
+    },
+    rules: {
+      // Relaxed rules for development purposes
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-require-imports': 'warn',
+    },
+  },
+];
